f0a3babe22cf72d77e5a9687fd6ae9df20b739c8
frankiechow <frankiechow08108@gmail.com>
2024-10-11 17:22:04 +0800

    update

--- demo-datatype/src/excercise1.java
+++ demo-datatype/src/excercise1.java
@@ -27,18 +27,67 @@ public class excercise1 {
     int year = input.nextInt();
 
     // Complete the switch statement
+   
     switch (month) {
       case 1:
         MonthOfName = "January";
-        // Code here
+        number_Of_DaysInMonth = 31;
+        break;
       case 2:
         MonthOfName = "February";
-        
+        if(month == 2 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {
+        number_Of_DaysInMonth = 29;
+        } else {
+        number_Of_DaysInMonth = 28;
+        }
+        break;
+      case 3:
+        MonthOfName = "March";
+        number_Of_DaysInMonth = 31;
+        break;
+      case 4:
+        MonthOfName = "April";
+        number_Of_DaysInMonth = 30;
+        break;
+      case 5:
+        MonthOfName = "May";
+        number_Of_DaysInMonth = 31;
+        break;
+      case 6:
+        MonthOfName = "June";
+        number_Of_DaysInMonth = 30;
+        break;
+      case 7:
+        MonthOfName = "July";
+        number_Of_DaysInMonth = 31;
+        break;
+      case 8:
+        MonthOfName = "August";
+        number_Of_DaysInMonth = 31;
+        break;
+      case 9:
+        MonthOfName = "September";
+        number_Of_DaysInMonth = 30;
+        break;
+      case 10:
+        MonthOfName = "October";
+        number_Of_DaysInMonth = 31;
+        break;
+      case 11:
+        MonthOfName = "November";
+        number_Of_DaysInMonth = 30;
+        break;
+      case 12:
+        MonthOfName = "December";
+        number_Of_DaysInMonth = 31;
+        break;
+      }
         // The February has 29 days: (Every 4 years and the year cannot divded by 100)
         // or The year can divided by 400
         // otherwise the February should have 28 days only
         // how about other months, December?
-    }
     System.out.print(MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
-    }
+    
+    
+  }
 }
